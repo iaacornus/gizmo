200ed1f3137f7727c2f65aab2b6eb9bc36955d19
iaacornus <iaacornus.devel@gmail.com>
2022-09-19 00:18:18 +0800

    update imports

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,12 +1,6 @@
-from os import getenv
-from os.path import dirname
+from discord import Client
 
-import discord
-
-from dotenv import load_dotenv
-
-
-class BotClient(discord.Client):
+class BotClient(Client):
     """Client for discord bot."""
 
     async def on_read(self) -> None:
@@ -14,18 +8,3 @@ class BotClient(discord.Client):
 
     async def on_message(self, message) -> None:
         print(message.content)
-
-
-def main() -> None:
-    BASE_PATH: str = "/".join(dirname(__file__).split("/")[:-2])
-    load_dotenv(f"{BASE_PATH}/bot.env")
-    token: str = getenv("TOKEN")
-
-    intents = discord.Intents.default()
-    intents.message_content = True
-
-    client = BotClient(intents=intents)
-    client.run(token)
-
-
-main()
