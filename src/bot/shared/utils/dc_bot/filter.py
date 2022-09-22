a0986b6f6342c18c7552951b01f941346596a098
iaacornus <iaacornus.devel@gmail.com>
2022-09-22 13:30:47 +0800

    shared modules and utils pkg

---
+++ src/bot/shared/utils/dc_bot/filter.py
@@ -0,0 +1,14 @@
+from src.utils.clog.clogger import Logger
+
+
+def filter(log: Logger, msg: str) -> list[str]:
+    """Determine the command based on message of the user.
+
+    Args:
+        log -- the instance of Logger
+        msg -- message content of the user
+
+    Returns:
+        The equivalent command and arguments.
+    """
+    ...
