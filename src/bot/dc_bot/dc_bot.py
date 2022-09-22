aa80716ffa6b7946ccc5afc795104c9cac368043
iaacornus <iaacornus.devel@gmail.com>
2022-09-22 15:40:06 +0800

    implement command filter

--- src/bot/dc_bot/dc_bot.py
+++ src/bot/dc_bot/dc_bot.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from discord import Client, Intents
 
+from src.bot.shared.utils.dc_bot.filter import filter
 from src.utils.clog.clogger import Logger
 
 
@@ -9,13 +10,23 @@ class BotClient(Client):
     """Client for discord bot."""
 
     def __init__(
-            self, log: Logger, *, intents: Intents, **options: Any
+            self,
+            log: Logger,
+            commands: dict[str, str | list[str]],
+            *,
+            intents: Intents,
+            **options: Any
         ) -> None:
         self.log = log
+        self.commands = commands
         super().__init__(intents=intents, **options)
 
     async def on_ready(self) -> None:
         self.log.logger("I", "Bot is ready.")
 
     async def on_message(self, message: Any) -> Any:
-        print(f"\033[36mMESS\033[0m\t {message.content}")
+        feedback: list[str] | str = filter(
+                self.log, message.content, self.commands
+            )
+        if feedback is not None:
+            await message.channel.send(feedback)
