ac6a0d206db51099cff52bf1c787b02b61d36201
iaacornus <iaacornus.devel@gmail.com>
2022-09-22 13:24:22 +0800

    create a subpackage for dc bot

---
+++ src/bot/dc_bot/dc_bot.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from discord import Client, Intents
+
+from src.utils.clog.clogger import Logger
+
+
+class BotClient(Client):
+    """Client for discord bot."""
+
+    def __init__(
+            self, log: Logger, *, intents: Intents, **options: Any
+        ) -> None:
+        self.log = log
+        super().__init__(intents=intents, **options)
+
+    async def on_ready(self) -> None:
+        self.log.logger("I", "Bot is ready.")
+
+    async def on_message(self, message: Any) -> Any:
+        print(f"\033[36mMESS\033[0m\t {message.content}")
---
+++ src/bot/utils/dc_bot_utils/fetch_bot_cred.py
@@ -0,0 +1,35 @@
+from os import getenv
+from os.path import dirname, exists
+from typing import NoReturn, Optional
+
+from dotenv import load_dotenv
+
+from src.data.dc_bot_cred import DCBotCred
+from src.utils.clog.clogger import Logger
+
+
+def fetch_bc(log: Logger) -> DCBotCred | NoReturn:
+    """Fetch the credentials of the bot in the .env file.
+
+    Args:
+        log -- the instance of Logger.
+
+    Returns:
+        The bot credentials with updated values.
+    """
+
+    BASE_PATH: str = "/".join(dirname(__file__).split("/")[:-3])
+    PATH: str = f"{BASE_PATH}/bot.env"
+
+    if not exists(PATH):
+        log.logger("E", "Bot credentials not found, aborting ...")
+        raise SystemExit
+
+
+    load_dotenv(f"{BASE_PATH}/bot.env")
+
+    token_: Optional[str]
+    token: str = (
+            token_ if (token_ := getenv("TOKEN")) is not None else "EMPTY"
+        )
+    return DCBotCred(token)
