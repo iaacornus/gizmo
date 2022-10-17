775d4a81acd5d839e246c85437ff340491278293
iaacornus <iaacornus.devel@gmail.com>
2022-10-18 00:15:46 +0800

    revert to a function and simplify the mechanism
    the internal function, _evlxec(), will only return a stdout or stderr
    which would be returned by filter() that will be sent to by the bot

--- src/bot/shared/utils/dc_bot/filter.py
+++ src/bot/shared/utils/dc_bot/filter.py
@@ -3,16 +3,31 @@ from subprocess import run, CalledProcessError
 from src.utils.clog.clogger import Logger
 
 
-class Filter:
-    def __init__(self, log: Logger) -> None:
-        self.log: Logger = log
+def filter(
+        log: Logger,
+        msg: str,
+        commands: dict[str, str | list[str]],
+        uid: int,
+        ref_uid: int
+    ) -> str | None:
+    """Determine the command based on message of the user.
+
+    Args:
+        log -- the instance of Logger
+        msg -- message content of the user
+        commands -- the reference for commands
+        uid -- user id
+        ref_uid -- reference user id
+
+    Returns:
+        Whether the message is a correct command and its feedback.
+    """
 
     def _evlxec(
-            self,
             msg: str,
             cmd: str,
             command: str
-        ) -> tuple[int, str]:
+        ) -> str:
         """Evaluate and execute the command.
 
         Args:
@@ -21,10 +36,7 @@ class Filter:
             command -- reference command
 
         Returns:
-            A particular return code for each cases:
-                0 - the user does not have enough permission.
-                1 - the command found and executed.
-                2 - the command not found and not executed.
+            A boolean and a stdout or stderr
         """
 
         if cmd == f"!{command.lower()}":
@@ -36,82 +48,26 @@ class Filter:
                     raise CalledProcessError
 
             except CalledProcessError as Err:
-                self.log.logger(
+                log.logger(
                     "I", f"Command failed: {command}, {exec_cmd}; {Err}"
                 )
             else:
-                self.log.logger(
+                log.logger(
                     "I", f"Command executed: {command}, {exec_cmd}"
                 )
-                return (1, f"Executed command: **{exec_cmd}**")
+                return f"Executed command: **{exec_cmd}**"
 
-        return (2, "Command not found.")
+        return "Command not found."
 
-    def _eval_retcode(
-            self,
-            ret_code: int,
-            feedback: str,
-            cmd: list[str]
-        ) -> tuple[bool, str]:
-        """Evaluate the return code and return the given tuple.
+    if not msg.startswith("!") or uid != ref_uid:
+        return None
 
-        Args:
-            ret_code -- the return code
-            feedback -- the feedback of evlxec
-            cmd -- the given command
+    cmd: str = msg.split()[0].lower()
 
-        Returns:
-            The tuple of a boolean value and a stdout or stderr.
-        """
-
-        match ret_code:
-            case 1:
-                self.log("I", feedback[1])
-                return True, feedback[1]
-            case 2:
-                self.log("e", feedback[1])
-                return False, feedback[1]
-            case 3:
-                stderr: str = f"Command: **{cmd}** not found, skipping"
-                self.log("e", stderr)
-                return False, stderr
-
-    def filter(
-            self,
-            msg: str,
-            commands: dict[str, str | list[str]],
-            uid: int,
-            ref_uid: int
-        ) -> tuple[bool, str] | None:
-        """Determine the command based on message of the user.
-
-        Args:
-            log -- the instance of Logger
-            msg -- message content of the user
-            uid -- user id
-
-            ref_uid -- reference user id
-        Returns:
-            Whether the message is a correct command and its feedback.
-        """
-
-        if not msg.startswith("!") or uid != ref_uid:
-            return None
-
-        cmd: str = msg.split()[0].lower()
-
-        command: str | list[str]; mcommand: str; output: tuple[bool, str]
-        for command in commands.values():
-            if isinstance(command, list):
-                for mcommand in command:
-                    return self._eval_retcode(
-                        self._evlxec(
-                            cmd, mcommand, uid, ref_uid
-                        )
-                    )
-            else:
-                return self._eval_retcode(
-                    self._evlxec(
-                        cmd, mcommand, uid, ref_uid
-                    )
-                )
+    command: str | list[str]; mcommand: str; output: tuple[bool, str]
+    for command in commands.values():
+        if isinstance(command, list):
+            for mcommand in command:
+                return _evlxec(cmd, mcommand, uid, ref_uid)
+        else:
+            return _evlxec(cmd, mcommand, uid, ref_uid)
