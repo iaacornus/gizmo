c55814089dfa9ed23949b8f4da78f875f801d409
iaacornus <iaacornus.devel@gmail.com>
2022-10-28 22:39:18 +0800

    update path and fix bugs

--- assets/misc/remove_spec_char.py
+++ assets/misc/remove_spec_char.py
@@ -3,16 +3,30 @@ from typing import TextIO
 
 
 def remove_spec_char() -> None:
-    BASE_PATH: str = "/".join(dirname(__file__))[:-2]
+    BASE_PATH: str = "/".join(dirname(__file__).split("/")[:-2])
     PATH: str = f"{BASE_PATH}/assets/templates/README"
 
     try:
         temp: TextIO
-        with open("proj_struct.temp", "r", encoding="UTF-8") as temp:
+        with open(
+                f"{BASE_PATH}/proj_struct.temp",
+                "r",
+                encoding="UTF-8"
+            ) as temp, open(
+                PATH,
+                "r",
+                encoding="UTF-8"
+            ) as template:
             lines: list[str] = temp.readlines()
+            readme_temp: list[str] = template.readlines()
 
         readme: TextIO; line: str
-        with open(PATH, "a", encoding="UTF-8") as readme:
+        with open(
+                f"{BASE_PATH}/README", "w", encoding="UTF-8"
+            ) as readme:
+            for line in readme_temp:
+                readme.write(line)
+
             for line in lines:
                 new_line: str = (
                         line
@@ -20,7 +34,11 @@ def remove_spec_char() -> None:
                             .replace("├──", "+--")
                             .replace("│  ", "|  ")
                     )
-                readme.write(f"{new_line}\n")
+                readme.write(new_line)
 
     except FileNotFoundError:
         pass
+
+
+if __name__ == "__main__":
+    remove_spec_char()
