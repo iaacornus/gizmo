333a6b36959913d2794d9641f8c204f8ebf7ea09
iaacornus <iaacornus.devel@gmail.com>
2022-11-14 20:58:07 +0800

    update script calls

--- update_readme.sh
+++ update_readme.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-assets/misc/./update_proj_structure.sh
-python assets/misc/replace_spec_char.py
+assets/misc/scripts/./update_proj_structure.sh
+python assets/misc/scripts/replace_spec_char.py
